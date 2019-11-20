e788e2f348af1ef044c0dc8c10addf48b5687bc8
programacion <>
2019-11-20 10:48:36 +0100

    con reordenacion de datos

--- src/app/ejercicio/ejercicio.page.ts
+++ src/app/ejercicio/ejercicio.page.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ejercicio.page.scss'],
 })
 export class EjercicioPage implements OnInit {
+  items: string[] = ["una", "DOS", "tres", "cuatrro"];
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  doReorder(ev: any) {
+    console.log('Before complete', this.items);
+    let element = this.items[ev.detail.from];
+    this.items.splice(ev.detail.from, 1);
+    this.items.splice(ev.detail.to, 0, element);
+    ev.detail.complete();
+    console.log('After complete', this.items);
+  }
 }
